ffb089ecc5484b0a11611aa019057a24222efa60
ltlylfun <funjee126@qq.com>
2025-05-14 20:23:31 +0800

    移除解析

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -20,10 +20,6 @@ export const TopNavBar = () => {
         en: "Build",
         zh: "构建",
       },
-      parse: {
-        en: "Parse",
-        zh: "解析",
-      },
       menu: {
         en: "Menu",
         zh: "菜单",
@@ -60,10 +56,7 @@ export const TopNavBar = () => {
           aria-label="Site Nav Bar"
           className="hidden items-center gap-2 text-sm font-medium md:flex"
         >
-          {[
-            ["/resume-builder", translate("build")],
-            ["/resume-parser", translate("parse")],
-          ].map(([href, text]) => (
+          {[["/resume-builder", translate("build")]].map(([href, text]) => (
             <Link
               key={text}
               className="rounded-md px-1.5 py-2 text-gray-500 hover:bg-gray-100 focus-visible:bg-gray-100 lg:px-4"
@@ -119,10 +112,7 @@ export const TopNavBar = () => {
         {menuOpen && (
           <div className="absolute left-0 right-0 top-[var(--top-nav-bar-height)] z-50 bg-white shadow-lg md:hidden">
             <div className="flex flex-col py-2">
-              {[
-                ["/resume-builder", translate("build")],
-                ["/resume-parser", translate("parse")],
-              ].map(([href, text]) => (
+              {[["/resume-builder", translate("build")]].map(([href, text]) => (
                 <Link
                   key={text}
                   onClick={closeMenu}
