8260d4b62fda1876e4e3400ba1635678e66028e8
Fang ZhiJie <funjee126@qq.com>
2025-06-14 12:02:38 +0800

    feat: 添加 metadataBase 字段

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,5 +1,6 @@
 export function getMetadata() {
   return {
+    metadataBase: new URL("https://resumetojob.ltlyl.fun/"),
     title: "ResumeToJob - 免费开源简历生成器 | Free Open-Source Resume Builder",
     description:
       "ResumeToJob是一个免费开源的简历制作网站，帮助您快速创建专业简历，支持多种模板和自定义选项。| ResumeToJob is a free and open-source resume builder that helps you create professional resumes quickly with multiple templates and customization options.",
