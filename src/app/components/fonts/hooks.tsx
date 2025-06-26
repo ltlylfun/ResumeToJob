94d0ec6b770d6ddad0f651f6606aa27a5c1ed358
Fang ZhiJie <funjee126@qq.com>
2025-06-26 22:27:32 +0800

    feat: 删除本地字体文件，更新字体引用为 CDN 链接以优化加载性能

--- src/app/components/fonts/hooks.tsx
+++ src/app/components/fonts/hooks.tsx
@@ -3,6 +3,28 @@ import { Font } from "@react-pdf/renderer";
 import { ENGLISH_FONT_FAMILIES } from "components/fonts/constants";
 import { getAllFontFamiliesToLoad } from "components/fonts/lib";
 
+export const getFontDetails = (fontFamily: string) => {
+  let fontsourceName = fontFamily.toLowerCase();
+  let subset = "latin";
+
+  switch (fontFamily) {
+    case "OpenSans":
+      fontsourceName = "open-sans";
+      break;
+    case "RobotoSlab":
+      fontsourceName = "roboto-slab";
+      break;
+    case "PlayfairDisplay":
+      fontsourceName = "playfair-display";
+      break;
+    case "NotoSansSC":
+      fontsourceName = "noto-sans-sc";
+      subset = "chinese-simplified";
+      break;
+  }
+  return { fontsourceName, subset };
+};
+
 /**
  * Register all fonts to React PDF so it can render fonts correctly in PDF
  */
@@ -10,15 +32,16 @@ export const useRegisterReactPDFFont = () => {
   useEffect(() => {
     const allFontFamilies = getAllFontFamiliesToLoad();
     allFontFamilies.forEach((fontFamily) => {
+      const { fontsourceName, subset } = getFontDetails(fontFamily);
       Font.register({
         family: fontFamily,
         fonts: [
           {
-            src: `/fonts/${fontFamily}-Regular.ttf`,
+            src: `https://cdn.jsdelivr.net/fontsource/fonts/${fontsourceName}@latest/${subset}-400-normal.ttf`,
             fontWeight: 400, // 使用数字400替代"normal"更加明确
           },
           {
-            src: `/fonts/${fontFamily}-Bold.ttf`,
+            src: `https://cdn.jsdelivr.net/fontsource/fonts/${fontsourceName}@latest/${subset}-700-normal.ttf`,
             fontWeight: 700, // 使用数字700替代"bold"更加明确
           },
         ],
@@ -41,7 +64,7 @@ export const useRegisterReactPDFHyphenationCallback = (fontFamily: string) => {
         word
           .split("")
           .map((char) => [char, ""])
-          .flat()
+          .flat(),
       );
     }
   }, [fontFamily]);
