ccd7e3a25e562e484215e558ce1e5f3669dab0f8
Fang ZhiJie <funjee126@qq.com>
2025-07-17 14:11:50 +0800

    feat: 添加元数据

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -6,6 +6,9 @@ export function getMetadata() {
       "ResumeToJob是一个免费开源的简历制作网站，帮助您快速创建专业简历，支持多种模板和自定义选项。| ResumeToJob is a free and open-source resume builder that helps you create professional resumes quickly with multiple templates and customization options.",
     keywords:
       "简历,开源简历,简历制作,简历生成器,Resume,Resume Builder,Open Source,Job,求职,模板,定制,多语言,free,open-source,professional,career,cv,template,customization",
+    verification: {
+      google: "L9NQe6I1sF3eQdwSwI-b0IE6-BifilZMg63Na7zMyCM",
+    },
     openGraph: {
       title:
         "ResumeToJob - 免费开源简历生成器 | Free Open-Source Resume Builder",
